619df027fc1d28e87bdc2c49c42c022279b0e01a
Fernando Lopez <fer.lopezg27@gmail.com>
2023-05-09 19:02:55 -0600

    init function imagen

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,6 +9,24 @@ const dropzone = new Dropzone('#dropzone',{
     dictRemoveFile:'Borrar fotografía',
     maxFiles:1,
     uploadMultiple:false,
+
+    init:function(){
+        if(document.querySelector('[name="imagen"]').value.trim()){
+            const imagenPublicada = {};
+            imagenPublicada.size = 1234;
+            imagenPublicada.name = document.querySelector('[name="imagen"]').value;
+            this.options.addedfile.call(this,imagenPublicada);
+            this.options.thumbnail.call(this,imagenPublicada,`/uploads/${imagenPublicada.name}`);
+            imagenPublicada.previewElement.classList.add(
+                "dz-success",
+                "dz-complete"
+            );
+        }
+    }
+});
+
+dropzone.on('removedfile',function(){
+    document.querySelector('[name="imagen"]').value = "";
 });
 
 dropzone.on('success',function(file,response){
